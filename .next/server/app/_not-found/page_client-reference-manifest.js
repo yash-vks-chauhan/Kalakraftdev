68b22d7c915d29c7a89ea381d1213f7162f050e8
yash-vks-chauhan <yash.vks.chauhan@gmail.com>
2025-06-26 18:47:02 +0530

    Initial commit

---
+++ .next/server/app/_not-found/page_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/_not-found/page"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/Users/yashchauhan/Artcommerce/":[],"/Users/yashchauhan/Artcommerce/app/_not-found/page":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}}
