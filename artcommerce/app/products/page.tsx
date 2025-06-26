03a4a49fed1214143224caf54057ffc089527375
yash-vks-chauhan <yash.vks.chauhan@gmail.com>
2025-06-26 20:53:54 +0530

    Disable prerender for products page to avoid useSearchParams SSR error

--- artcommerce/app/products/page.tsx
+++ artcommerce/app/products/page.tsx
@@ -2,6 +2,8 @@
 
 'use client'
 
+export const dynamic = 'force-dynamic';
+
 import { useState, useEffect, useRef } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
