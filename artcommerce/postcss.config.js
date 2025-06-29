1938d713fcc16be47758a1818a2cc3a13fb867bc
yash-vks-chauhan <yash.vks.chauhan@gmail.com>
2025-06-29 09:58:09 +0530

    Fix Browserslist configuration and PostCSS setup

--- artcommerce/postcss.config.js
+++ artcommerce/postcss.config.js
@@ -1,8 +1,7 @@
 // PostCSS configuration
 module.exports = {
-  plugins: {
-    'tailwindcss/nesting': {},
-    'tailwindcss': require.resolve('tailwindcss'),
-    'autoprefixer': require.resolve('autoprefixer'),
-  }
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+  ],
 } 
