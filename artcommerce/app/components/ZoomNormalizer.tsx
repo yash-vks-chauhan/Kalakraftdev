c2ccfca0dfe66276be83f12ecb73dc8084abc580
yash-vks-chauhan <yash.vks.chauhan@gmail.com>
2025-07-04 12:29:16 +0530

    feat: always apply 80% scale on desktop

--- artcommerce/app/components/ZoomNormalizer.tsx
+++ artcommerce/app/components/ZoomNormalizer.tsx
@@ -5,10 +5,8 @@ import { useEffect } from 'react'
 export default function ZoomNormalizer() {
   useEffect(() => {
     const normalize = () => {
-      const zoomPercent = (window.outerWidth / window.innerWidth) * 100
-      const target = 80
-      if (window.innerWidth >= 768 && zoomPercent > 100) {
-        const scale = target / zoomPercent
+      if (window.innerWidth >= 768) {
+        const scale = 0.8
 
         document.documentElement.style.transformOrigin = '0 0'
         document.documentElement.style.transform = `scale(${scale})`
