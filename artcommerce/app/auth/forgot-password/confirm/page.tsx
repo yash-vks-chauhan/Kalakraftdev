4b98890310b7080ee77bd54dc1627a0c8c5a2e2f
yash-vks-chauhan <yash.vks.chauhan@gmail.com>
2025-07-04 09:50:04 +0530

    Disable prerender on forgot-password confirm page to handle useSearchParams hook

--- artcommerce/app/auth/forgot-password/confirm/page.tsx
+++ artcommerce/app/auth/forgot-password/confirm/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic';
 import { useSearchParams, useRouter } from 'next/navigation'
 import PasswordChangeForm             from '../../../components/PasswordChangeForm'
 
